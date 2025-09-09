427de93d0ac770022ba681ae563306fd68e5519f
manuela121212 <manuela180707@gmail.com>
2025-09-09 17:29:11 -0500

    mensaje

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -128,7 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configuración de email
